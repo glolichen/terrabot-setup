33dfa143efee81217f3e4a09c167f03d86597bef
glolichen <jlche18659@gmail.com>
2024-04-12 08:41:24 -0400

    initial commit

---
+++ setup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+sudo apt install dphys-swapfile`
+sudo echo "CONF_SWAPFILE=/var/swap" | /etc/dphys-swapfile
+sudo dphys-swapfile setup
+sudo dphys-swapfile swapon
+
+sudo systemctl enable ssh
+sudo systemctl start ssh
+
+sudo apt update
+sudo apt upgrade
+sudo apt install python3 python3-pip git
+sudo apt install python-is-python3
+sudo apt install vlc
+pip install matplotlib tmux transitions scikit-learn opencv-python
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt install curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt install build-essential arduino arduino-mk
+sudo apt install ros-noetic-rosserial ros-noetic-rosserial-arduino
+echo "source /opt/ros/noetic/setup.bash" | ~/.bashrc
+
+cd Desktop
+git clone https://github.com/reidgs/TerraBot
+cd $HOME
+ln -s Desktop/TerraBot .
+echo "source /opt/ros/noetic/setup.bash" | ~/.bashrc
+echo "export TB_DIR=${HOME}/Desktop/TerraBot" | ~/.bashrc
+echo "export export PYTHONPATH=${PYTHONPATH}:${TB_DIR}:${TB_DIR}/lib/:${TB_DIR}:${TB_DIR}/agents/" | ~/.bashrc
+
+ln -s Desktop/TerraBot .
+mkdir ~/Arduino
+mkdir ~/Arduino/libraries
+cd ~/Arduino/libraries
+rosrun rosserial_arduino make_libraries.py .
+git clone https://github.com/RobTillaart/DHT20
+git clone https://github.com/RobTillaart/HX711
+git clone https://github.com/RobTillaart/TCA9548
+git clone https://github.com/RobTillaart/dhtnew
+sudo usermod -a -G dialout,robotanist $USER
+sudo usermod -a -G dialout robotanist
+echo "export PATH=$PATH:/home/robotanist:/home/robotanist/.local/bin/:/root/.local/bin/:/home/robotanist/arduino-cli/bin/" | ~/.bashrc
+
+arduino-cli config init
+arduino-cli core update-index
+arduino-cli core install arduino:avr
+arduino-cli compile --fqbn arduino:avr:mega ArduinoCode.ino
+arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:mega ArduinoCode.ino
+
+pip install ortools
+
+cd /home/robotanist/.local/lib/
+sudo cp -r python3.8/site-packages/* /usr/lib/python3/dist-packages/.
+sudo rm -rf python3.8
+
+cd ~/Desktop/TerraBot
